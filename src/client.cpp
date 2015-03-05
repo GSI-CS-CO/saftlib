bd07aa944bc92d152812ae309ddb867fc6c64925
Wesley W. Terpstra <w.terpstra@gsi.de>
2015-03-05 20:38:17 +0100

    client: demonstrate ECA

--- src/client.cpp
+++ src/client.cpp
@@ -3,6 +3,11 @@
 #include <iostream>
 #include <giomm.h>
 #include "interfaces/TLU.h"
+#include "interfaces/ECA.h"
+#include "interfaces/ECA_Condition.h"
+
+// something like this will move into final library
+#define create_(x) create_for_bus_sync(Gio::DBus::BUS_TYPE_SESSION, "de.gsi.saftlib", x)
 
 Glib::RefPtr<saftlib::TLU_Proxy> channel;
 
@@ -12,16 +17,21 @@ void on_edge(const guint64& time) {
   std::cout << "Pulse detected: " << time << " @ " << now << " = " << (now-time) << std::endl;
 }
 
+void on_action(
+  const guint64& id, const guint64& param, const guint64& time, 
+  const guint32& tef, const bool& late, const bool& conflict)
+{
+  std::cout << "Saw a timing event!" << std::endl;
+}
+
 int main(int, char**)
 {
   Gio::init();
-
-  try {
+  auto loop = Glib::MainLoop::create();
   
-    Glib::RefPtr<Glib::MainLoop> loop = Glib::MainLoop::create();
-    channel =
-      saftlib::TLU_Proxy::create_for_bus_sync(
-        Gio::DBus::BUS_TYPE_SESSION, "de.gsi.saftlib", "/de/gsi/saftlib/TLU_pex0_100/in_0");
+  try {
+    // Play with the TLU
+    channel = saftlib::TLU_Proxy::create_("/de/gsi/saftlib/TLU_pex0_100/in_0");
     
     // Was it already active?
     std::cout << "Channel was: " << (channel->getEnabled()?"active":"inactive") << std::endl;
@@ -31,8 +41,19 @@ int main(int, char**)
     channel->setLatchEdge(false);
     channel->setStableTime(16);
     channel->setEnabled(true);
+    
+    // Play with the ECA
+    auto eca = saftlib::ECA_Proxy::create_("/de/gsi/saftlib/ECA_pex0_80");
+    
+    // Create a condition
+    Glib::ustring path;
+    eca->NewCondition(5, 100, 100, path);
+    auto condition = saftlib::ECA_Condition_Proxy::create_(path);
+  
+    // Watch the condition
+    condition->Action.connect(sigc::ptr_fun(&on_action));
 
-    std::cout << "Waiting for edges" << std::endl;
+    std::cout << "Waiting for TLU edges or ECA events" << std::endl;
     loop->run();
     
   } catch (const Glib::Error& error) {
