afc5086f80b5364f5b4990b329aa419c6b4dfc02
Wesley W. Terpstra <w.terpstra@gsi.de>
2015-05-27 15:14:12 +0200

    client: update demo to use new API

--- src/client.cpp
+++ src/client.cpp
@@ -1,22 +1,17 @@
-#define ETHERBONE_THROWS 1
-
 #include <iostream>
 #include <giomm.h>
+
 #include "interfaces/Directory.h"
-#include "interfaces/TLU.h"
-#include "interfaces/ECA.h"
-#include "interfaces/ECA_Condition.h"
+#include "interfaces/TimingReceiver.h"
+#include "interfaces/SoftwareActionSink.h"
+#include "interfaces/SoftwareCondition.h"
 
-Glib::RefPtr<saftlib::TLU_Proxy> channel;
-void on_edge(guint64 time) {
-  guint64 now = channel->CurrentTime();
-  std::cout << "Pulse detected: " << time << " @ " << now << " = " << (now-time) << std::endl;
-}
-
-void on_action(guint64 id, guint64 param, guint64 time, guint32 tef, bool late, bool conflict)
+void on_action(guint64 id, guint64 param, guint64 time, bool late, bool conflict)
 {
   std::cout << "Saw a timing event!" << std::endl;
 }
+
+using namespace saftlib;
 
 int main(int, char**)
 {
@@ -25,36 +20,23 @@ int main(int, char**)
   
   try {
     // Open the saftlib directory
-    std::map< Glib::ustring, std::vector< Glib::ustring > > devices = 
-      saftlib::Directory_Proxy::create()->getDevices();
+    auto devices = Directory::create()->getDevices();
     
-    // Play with the TLU
-    channel = saftlib::TLU_Proxy::create(devices["TLU"][0]);
+    // Grab a handle to the timing receiver
+    auto receiver = TimingReceiver::create(devices["backplane"]);
     
-    // Was it already active?
-    std::cout << "Channel was: " << (channel->getEnabled()?"active":"inactive") << std::endl;
+    // Make a new software action sink
+    auto sink = SoftwareActionSink::create(receiver->NewSoftwareActionSink(""));
     
-    // Listen for edges
-    channel->Edge.connect(sigc::ptr_fun(&on_edge));
-    channel->setLatchEdge(false);
-    channel->setStableTime(16);
-    channel->setEnabled(true);
-    
-    // Play with the ECA
-    Glib::RefPtr<saftlib::ECA_Proxy> eca = 
-      saftlib::ECA_Proxy::create(devices["ECA"][0]);
-    
-    // Create a condition, watching events 5-200 delayed by +100*8 nanoseconds
-    Glib::RefPtr<saftlib::ECA_Condition_Proxy> condition = 
-      saftlib::ECA_Condition_Proxy::create(
-        eca->NewCondition(5, 200, 100));
+    // Create an active(true) condition, watching events 5-200 delayed by 100 nanoseconds
+    auto condition = SoftwareCondition::create(sink->NewCondition(true, 5, 200, 100, 0));
   
-    // Watch the condition
+    // Call on_action whenever the condition is true
     condition->Action.connect(sigc::ptr_fun(&on_action));
 
-    std::cout << "Waiting for TLU edges or ECA events" << std::endl;
+    std::cout << "Waiting for timing events" << std::endl;
     loop->run();
-    
+
   } catch (const Glib::Error& error) {
     std::cerr << "Failed to invoke method: " << error.what() << std::endl;
   }
