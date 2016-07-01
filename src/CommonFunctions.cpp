064a78199c73c4ad4d437d51e9a92a929ed76a33
Dietrich Beck <d.beck@gsi.de>
2016-07-01 15:54:33 +0200

    extended CommonFunctions, CommonFunctions now used by saft-ctl

--- src/CommonFunctions.cpp
+++ src/CommonFunctions.cpp
@@ -1,22 +1,94 @@
 #include "CommonFunctions.h"
 
+using namespace std;
+
 /* Format mask for action sink */
 guint64 tr_mask(int i)
 {
   return i ? (((guint64)-1) << (64-i)) : 0;
-}
+} //tr_mask
 
-/* Format date to a human-readable string */
-const char* tr_formatDate(guint64 time)
+// 
+std::string tr_formatDate(guint64 time, guint32 pmode)
 {
   guint64 ns    = time % 1000000000;
   time_t  s     = time / 1000000000;
   struct tm *tm = gmtime(&s);
   char date[40];
-  static char full[80];
-  
-  strftime(date, sizeof(date), "%Y-%m-%d %H:%M:%S", tm);
-  snprintf(full, sizeof(full), "%s.%09ld", date, (long)ns);
+  char full[80];
+  string temp;
 
-  return full;
-}
+  if (pmode & PMODE_VERBOSE) {
+	strftime(date, sizeof(date), "%Y-%m-%d %H:%M:%S", tm);
+	snprintf(full, sizeof(full), "%s.%09ld", date, (long)ns);
+  }
+  else if (pmode & PMODE_DEC) snprintf(full, sizeof(full), "0d%lu.%09ld",s,(long)ns);
+  else snprintf(full, sizeof(full), "0x%016" PRIx64, time);
+
+  temp = full;
+
+  return temp;
+} //tr_formatDate
+
+/* format EvtID to a string */
+std::string tr_formatActionEvent(guint64 id, guint32 pmode)
+{
+  std::stringstream full;
+  string fmt = "";
+  int width = 0;
+
+  if (pmode & PMODE_HEX) {full << std::hex << std::setfill('0'); width = 16; fmt = "0x";}
+  if (pmode & PMODE_DEC) {full << std::dec << std::setfill('0'); width = 20; fmt = "0d";}
+  if (pmode & PMODE_VERBOSE) {
+	full << " FID: "   << fmt << std::setw(1) << ((id >> 60) & 0xf);
+    full << " GID: "   << fmt << std::setw(4) << ((id >> 48) & 0xfff);
+	full << " EVTNO: " << fmt << std::setw(4) << ((id >> 36) & 0xfff);
+	full << " SID: "   << fmt << std::setw(4) << ((id >> 24) & 0xfff);
+	full << " BPID: "  << fmt << std::setw(5) << ((id >> 14) & 0x3fff);
+	full << " RES: "   << fmt << std::setw(4) << (id & 0x3ff);
+  }
+  else full << " EvtID: " << fmt << std::setw(width) << std::setfill('0') << id;
+
+  return full.str();
+} //tr_formatActionEvent
+
+std::string tr_formatActionParam(guint64 param, guint32 evtNo, guint32 pmode)
+{
+  std::stringstream full;
+  string fmt = "";
+  int width = 0;
+
+  if (pmode & PMODE_HEX) {full << std::hex << std::setfill('0'); width = 16; fmt = "0x";}
+  if (pmode & PMODE_DEC) {full << std::dec << std::setfill('0'); width = 20; fmt = "0d";}
+  
+  switch (evtNo) {
+  case 0x0 :
+	// add some code
+	break;
+  case 0x1 :
+	// add some code
+	break;
+  default :
+	full << " Param: " << fmt << std::setw(width) << param;
+  } // switch evtNo
+
+  return full.str();
+} // tr_formatActionParam
+
+std::string tr_formatActionFlags(guint16 flags, guint64 delay, guint32 pmode)
+{
+  std::stringstream full;
+
+  full << "";
+
+  if (flags) {
+    full  << "!";
+    if (flags & 1) full << "late (by " << delay << " ns)";
+    if (flags & 2) full << "early (by " << -delay << " ns)";
+    if (flags & 4) full << "conflict (delayed by " << delay << " ns)";
+    if (flags & 8) full << "delayed (by " << delay << " ns)";
+  } // if flags
+
+  return full.str();
+} // tr_formatActionFlags
+
--- src/CommonFunctions.h
+++ src/CommonFunctions.h
@@ -9,9 +9,40 @@
 #include <sys/time.h>
 #include <stdio.h>
 #include <string.h>
+#include <sstream>
 #include <inttypes.h>
 
-guint64     tr_mask(int i);
-const char* tr_formatDate(guint64 time);
+// modes for printing to cout
+const guint32 PMODE_NONE     = 0x0;
+const guint32 PMODE_DEC      = 0x1;
+const guint32 PMODE_HEX      = 0x2;
+const guint32 PMODE_VERBOSE  = 0x4;
+
+// formatting of mask for action sink
+guint64     tr_mask(int i                    	  // number of bits
+					);
+
+// formatting of date for output
+std::string tr_formatDate(guint64 time,      	  // time [ns]
+						  guint32 pmode      	  // mode for printing
+						  );
+
+// formatting of action event ID for output
+std::string tr_formatActionEvent(guint64 id,  	  // 64bit event ID
+								 guint32 pmode	  // mode for printing
+								 );
+
+// formatting of action param for output; the format depends also on evtNo, except if evtNo == 0xFFFF FFFF
+std::string tr_formatActionParam(guint64 param,    // 64bit parameter
+								 guint32 evtNo,    // evtNo (currently 12 bit) - part of the 64 bit event ID
+								 guint32 pmode     // mode for printing
+								 );
+
+// formatting of action flags for output
+std::string tr_formatActionFlags(guint16 flags,    // 16bit flags
+								 guint64 delay,    // used in case action was delayed
+								 guint32 pmode     // mode for printing
+								 );
+
 
 #endif /* #ifndef COMMON_FUNCTIONS_H */
