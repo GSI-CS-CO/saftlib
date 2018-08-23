2a9f6dd0e38adee81c9e0e6676a1ee1c64eaf20e
Michael Reese <m.reese@gsi.de>
2018-08-23 11:07:18 +0200

    saftbus: fix indentation

--- saftbus/Socket.cpp
+++ saftbus/Socket.cpp
@@ -14,94 +14,94 @@ namespace saftbus
 
 void Socket::wait_for_client()
 {
-  _active = false;
-  struct timespec start, stop;
-  clock_gettime( CLOCK_REALTIME, &start);
-  _create_socket = socket(AF_LOCAL, SOCK_SEQPACKET, 0);
-  if (_create_socket > 0) {
-    if (_debug_level > 5) std::cerr << "socket created" << std::endl;
-  } // TODO: else { ... }
-  unlink(_filename.c_str());
-  _address.sun_family = AF_LOCAL;
-  strcpy(_address.sun_path, _filename.c_str());
-  if (_debug_level > 5) std::cerr << "bind to file " << _filename << std::endl;
-  int bind_result = bind (_create_socket, (struct sockaddr *)&_address, sizeof(_address) );
-  if (bind_result != 0) {
-    if (_debug_level > 5) std::cerr << "port busy" << std::endl;
-    throw std::runtime_error("port busy");
-  }
-  listen(_create_socket, 1);
-  //_addrlen = sizeof(struct sockaddr_in);
-  // the connection will wait for incoming calls
-  try {
-    Glib::signal_io().connect(sigc::mem_fun(*this, &Socket::accept_connection), _create_socket, Glib::IO_IN | Glib::IO_HUP, Glib::PRIORITY_LOW);
-  } catch(std::exception &e)
-  {
-    if (_debug_level > 5) std::cerr << "wait_for_client exception: " << e.what() << std::endl;
-    throw;
-  }
-  clock_gettime( CLOCK_REALTIME, &stop);
-  double dt = (1.0e6*stop.tv_sec   + 1.0e-3*stop.tv_nsec) 
-            - (1.0e6*start.tv_sec + 1.0e-3*start.tv_nsec);
+	_active = false;
+	struct timespec start, stop;
+	clock_gettime( CLOCK_REALTIME, &start);
+	_create_socket = socket(AF_LOCAL, SOCK_SEQPACKET, 0);
+	if (_create_socket > 0) {
+		if (_debug_level > 5) std::cerr << "socket created" << std::endl;
+	} // TODO: else { ... }
+	unlink(_filename.c_str());
+	_address.sun_family = AF_LOCAL;
+	strcpy(_address.sun_path, _filename.c_str());
+	if (_debug_level > 5) std::cerr << "bind to file " << _filename << std::endl;
+	int bind_result = bind (_create_socket, (struct sockaddr *)&_address, sizeof(_address) );
+	if (bind_result != 0) {
+		if (_debug_level > 5) std::cerr << "port busy" << std::endl;
+		throw std::runtime_error("port busy");
+	}
+	listen(_create_socket, 1);
+	//_addrlen = sizeof(struct sockaddr_in);
+	// the connection will wait for incoming calls
+	try {
+		Glib::signal_io().connect(sigc::mem_fun(*this, &Socket::accept_connection), _create_socket, Glib::IO_IN | Glib::IO_HUP, Glib::PRIORITY_LOW);
+	} catch(std::exception &e)
+	{
+		if (_debug_level > 5) std::cerr << "wait_for_client exception: " << e.what() << std::endl;
+		throw;
+	}
+	clock_gettime( CLOCK_REALTIME, &stop);
+	double dt = (1.0e6*stop.tv_sec   + 1.0e-3*stop.tv_nsec) 
+						- (1.0e6*start.tv_sec + 1.0e-3*start.tv_nsec);
 
-  if (_debug_level > 5) std::cerr << "wait_for_client dt = " << dt << " us" << std::endl;          
+	if (_debug_level > 5) std::cerr << "wait_for_client dt = " << dt << " us" << std::endl;          
 
 }
 
 void Socket::close_connection()
 {
-  close(_new_socket);
+	close(_new_socket);
 }
 
 bool Socket::get_active()
 {
-  return _active;
+	return _active;
 }
 
 std::string Socket::get_filename()
 {
-  return _filename;
+	return _filename;
 }
 
 Socket::Socket(const std::string &name, Connection *server_connection)
-  : _filename(name)
-  , _server_connection(server_connection)
-  , _active(false)
+	: _filename(name)
+	, _server_connection(server_connection)
+	, _active(false)
 
 {
-  // // the connection will wait for incoming calls
-  // Glib::signal_io().connect(sigc::mem_fun(*this, &Socket::accept_connection), _create_socket, Glib::IO_IN | Glib::IO_HUP, Glib::PRIORITY_LOW);
-  wait_for_client();
+	// // the connection will wait for incoming calls
+	// Glib::signal_io().connect(sigc::mem_fun(*this, &Socket::accept_connection), _create_socket, Glib::IO_IN | Glib::IO_HUP, Glib::PRIORITY_LOW);
+	wait_for_client();
 }
 
 bool Socket::accept_connection(Glib::IOCondition condition)
 {
-  _addrlen = sizeof(struct sockaddr_in);
-  _new_socket = accept(_create_socket, (struct sockaddr*) &_address, &_addrlen);
-  //std::cerr << "listen = " << listen(_create_socket, 0) << std::endl;
-  close(_create_socket);
-  if (_new_socket > 0)
-  {
-    if (_debug_level > 5) std::cerr << "client connected " << std::endl;
-  }
-  Glib::signal_io().connect(sigc::bind(sigc::mem_fun(_server_connection, &Connection::dispatch), this), _new_socket, Glib::IO_IN | Glib::IO_HUP, Glib::PRIORITY_HIGH);
-  _active = true;
-  return false; // MainLoop should stop watching _create_socket file descriptor
+	_addrlen = sizeof(struct sockaddr_in);
+	_new_socket = accept(_create_socket, (struct sockaddr*) &_address, &_addrlen);
+	//std::cerr << "listen = " << listen(_create_socket, 0) << std::endl;
+	close(_create_socket);
+	if (_new_socket > 0)
+	{
+		if (_debug_level > 5) std::cerr << "client connected " << std::endl;
+	}
+	Glib::signal_io().connect(sigc::bind(sigc::mem_fun(_server_connection, &Connection::dispatch), this), _new_socket, Glib::IO_IN | Glib::IO_HUP, Glib::PRIORITY_HIGH);
+	_active = true;
+	return false; // MainLoop should stop watching _create_socket file descriptor
 }
 
 int Socket::get_fd()
 {
-  return _new_socket;
+	return _new_socket;
 }
 Glib::ustring& Socket::saftbus_id()
 {
-  return _saftbus_id;
+	return _saftbus_id;
 }
 
 
 Socket::~Socket()
 {
-  close(_create_socket);
+	close(_create_socket);
 }
 
 } // namespace saftbus
