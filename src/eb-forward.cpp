bddba9b442b96fab14c4390abbed0d9e8e8af576
Michael Reese <m.reese@gsi.de>
2019-11-06 14:04:49 +0100

    saftd: cleanup code

--- src/eb-forward.cpp
+++ src/eb-forward.cpp
@@ -14,7 +14,6 @@ namespace saftlib {
 		if (_eb_device_fd == -1) {
 			return;
 		}
-		_write_buffer_length = 0;
 	} 
 	EB_Forward::~EB_Forward()
 	{
@@ -34,15 +33,13 @@ namespace saftlib {
 			uint8_t val;
 			int result = read(_pts_fd, (void*)&val, sizeof(val));
 			if (result == 0) { // end of file 
-				// std::cerr << "end of file" << std::endl;
 				return true;
 			}
-			if (result == -1) {
+			if (result == -1) { // error
 				std::cerr << "EB_Forward error(" << std::dec << errno << "): " << strerror(errno) << std::endl;
 				return true;
 			}
-			if (result == sizeof(val)) {
-				// std::cerr << "<<<< " << std::setfill('0') << std::setw(2) << std::hex << (int)val << std::endl;
+			if (result == sizeof(val)) { // normal read 
 				request.push_back(val);
 
 				if (request.size() == 4) {
@@ -50,7 +47,6 @@ namespace saftlib {
 						request[1]     == 0x6f &&
 						request[2]     == 0x11 &&
 						request[3]     == 0xff) {
-						// std::cerr << "etherbone header detected" << std::endl;
 
 						response.push_back(0x4e);
 						response.push_back(0x6f);
@@ -59,48 +55,36 @@ namespace saftlib {
 
 						for (int i = 0; i < 4; ++i) {
 							read(_pts_fd, (void*)&val, sizeof(val));
-							// std::cerr << "<<<< " << std::setfill('0') << std::setw(2) << std::hex << (int)val << std::endl;
 							response.push_back(val);
 						}
-						// for (int i = 0; i < response.size(); ++i) {
-						// 	std::cerr << ">>>> " << std::setfill('0') << std::setw(2) << std::hex << (int)response[i] << std::endl;
-						// }
 						write(_pts_fd, (void*)&response[0], response.size());
 						// handling of Etherbone header done.
 						return true;
 					} else { // assume it is an Etherbone cycle header, calculate the response size
-						//std::cerr << "cycle header detected" << std::endl;
 						int wcount = request[2];
 						int rcount = request[3];
-						request_size += 4;
+						request_size += 4; // for the cycle header
 						if (wcount) {
-							request_size += 4*(1+wcount);
+							request_size += 4 + 4*wcount; // base address + wcount write values 
 						}
 						if (rcount) {
-							request_size += 4*(1+rcount);
+							request_size += 4 + 4*rcount; // base return address + rcount read addresses
 						}
-						// std::cerr << "  wcount=" << std::dec << wcount 
-						// 		  << "  rcount=" << std::dec << rcount 
-						//           << "  request size = " << std::dec << request_size << std::endl;
 					}
 				}
 
 				// process the request by sending it to the device,
 				//  read the response from the device and write the 
-				//  response back to the eb-tool
+				//   response back to the eb-tool
 				if (request_size && request_size == request.size()) {
 					// all cycle bytes read
 					write(_eb_device_fd, (void*)&request[0], request.size());
 					response.clear();
 					response.resize(request.size());
 					read(_eb_device_fd, (void*)&response[0], response.size());
-					// for (int i = 0; i < response.size(); ++i) {
-					// 	std::cerr << ">>>> " << std::setfill('0') << std::setw(2) << std::hex << (int)response[i] << std::endl;
-					// }
 					write(_pts_fd, (void*)&response[0], response.size());
 					return true;
 				}
-
 
 			}
 		}
