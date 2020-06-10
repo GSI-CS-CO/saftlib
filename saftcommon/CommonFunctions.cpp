d196eac9ceef6d58a647a8bfbcc2bd9fe3889e30
Martin Skorsky <m.skorsky@gsi.de>
2020-06-10 14:44:30 +0200

    flag Beamprocess Cheain start: show as BPC with snoop

--- saftcommon/CommonFunctions.cpp
+++ saftcommon/CommonFunctions.cpp
@@ -70,6 +70,7 @@ std::string tr_formatActionEvent(uint64_t id, uint32_t pmode)
     } // if fid==0
     if (fid == 1) {
       full << " FLAGS: " << fmt << std::setw(1) << ((id >> 32) & 0xf);
+      full << " BPC: "   << fmt << std::setw(1) << ((id >> 34) & 0x1);
       full << " SID: "   << fmt << std::setw(4) << ((id >> 20) & 0xfff);
       full << " BPID: "  << fmt << std::setw(5) << ((id >> 6) & 0x3fff);
       full << " RES: "   << fmt << std::setw(4) << (id & 0x3f);
