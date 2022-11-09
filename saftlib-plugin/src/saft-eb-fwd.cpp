29e5d6bd769fe395f96dc268fb763952d08e6b40
Michael Reese <m.reese@gsi.de>
2022-11-09 09:07:28 +0100

    file headers

--- saftlib-plugin/src/saft-eb-fwd.cpp
+++ saftlib-plugin/src/saft-eb-fwd.cpp
@@ -1,3 +1,36 @@
+// @brief Retrieve the name of the etherbone path of a logical saftlib device (eg. tr0)
+// @author Michael Reese  <m.reese@gsi.de>
+//
+// Copyright (C) 2015 GSI Helmholtz Centre for Heavy Ion Research GmbH
+//
+// Some etherbone devices support only a single connection. If saftlib
+// connects to such a device, it will open a pseudo-terminal (e.g. /dev/pts/<number>).
+// It will listen to data on the pseudo-terminal and make it behave like
+// a real etherbone slave by redirecting the traffic to the connected hardware
+// and deliver the response.
+//
+// Usage scenario: saftlib is connected to /dev/ttyUSB0 under logical name tr0.
+//                 If "eb-ls dev/ttyUSB0" would be called now, both saftlib and
+//                 the device will crash, because the serial driver doesn't 
+//                 support multiple connections.
+//                 If instead "eb-ls $(saft-eb-fwd tr0)" is called, eb-ls will
+//                 work as expected.
+//
+//*****************************************************************************
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//*****************************************************************************
+//
 #include "interfaces/SAFTd.h"
 #include <iostream>
 
