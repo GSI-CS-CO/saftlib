d69acbb476e1cb70a6db11e594122a17e0ff30d7
Michael Reese <m.reese@gsi.de>
2023-01-19 11:44:36 +0100

    improve roundtrip measurment tools

--- saftlib-plugin/src/saft-standalone-roundtrip-latency.cpp
+++ saftlib-plugin/src/saft-standalone-roundtrip-latency.cpp
@@ -2,6 +2,7 @@
 #include "TimingReceiver.hpp"
 #include "SoftwareActionSink.hpp"
 #include "SoftwareCondition.hpp"
+#include "CommonFunctions.hpp"
 
 #include <iostream>
 #include <fstream>
@@ -11,18 +12,13 @@
 #include <map>
 
 std::chrono::time_point<std::chrono::steady_clock> start, stop;
-
 std::vector<int> histogram(10000);
-
-std::shared_ptr<saftlib::SAFTd>          saftd;
-std::shared_ptr<saftlib::TimingReceiver> tr;
 
 
 static void on_action(uint64_t id, uint64_t param, saftlib::Time deadline, saftlib::Time executed, uint16_t flags)
 {
 	stop = std::chrono::steady_clock::now();
 	std::chrono::nanoseconds duration = stop-start;
-	// std::cerr << "on_action " << duration.count() << std::endl;
 	int us = duration.count()/1000;
 	if (us >= 0 && us < (int)histogram.size()) {
 		histogram[us]++;
@@ -30,13 +26,13 @@ static void on_action(uint64_t id, uint64_t param, saftlib::Time deadline, saftl
 } 
 
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		std::cerr << "usage: " << argv[0] << " <eb-device>" << std::endl;
+	if (argc != 4) {
+		std::cerr << "usage: " << argv[0] << " <eb-device> <number-of-measurements> <histogram-filename>" << std::endl;
 		return 1;
 	}
 	try {
-		saftd = std::make_shared<saftlib::SAFTd>();
-		tr    = std::make_shared<saftlib::TimingReceiver>(*saftd, "tr0", argv[1]);
+		auto saftd = std::make_shared<saftlib::SAFTd>();
+		auto tr    = std::make_shared<saftlib::TimingReceiver>(*saftd, "tr0", argv[1]);
 
 		auto software_action_sink_object_path = tr->NewSoftwareActionSink("");
 		auto software_action_sink             = tr->getSoftwareActionSink(software_action_sink_object_path);
@@ -48,15 +44,22 @@ int main(int argc, char *argv[]) {
 		condition->setAcceptDelayed(true);
 		condition->SigAction.connect(sigc::ptr_fun(&on_action));
 
-		for (int i = 0; i < 10000; ++i) {
-			auto now = tr->CurrentTime();
-			start = std::chrono::steady_clock::now();
-			tr->InjectEvent(0xaffe, 0x0, now);
-			saftbus::Loop::get_default().iteration(true);
-			//saftbus::SignalGroup::get_global().wait_for_signal();
+		int N;
+		std::istringstream Nin(argv[2]);
+		Nin >> N;
+		if (!Nin) {
+			std::cerr << "cannot read number-of-measurements from " << argv[2] << std::endl;
+			return 1;
 		}
 
-		std::ofstream hist("histogram.dat");
+		for (int i = 0; i < N; ++i) {
+			start = std::chrono::steady_clock::now();
+			tr->InjectEvent(0xaffe, 0x0, saftlib::makeTimeTAI(0));
+			saftbus::Loop::get_default().iteration(true);
+		}
+
+		std::ofstream hist(argv[3]);
+		hist << "# time[us] counts" << std::endl;
 		for (unsigned i = 0 ; i < histogram.size() ; ++i) {
 			hist << i << " " << histogram[i] << std::endl;
 		}
