55c32f6f2c6205de23f0f57ee962c183cb292e07
Michael Reese <m.reese@gsi.de>
2023-04-03 11:50:13 +0200

    add saftbus.h compatibility header and include saftbus/errror.hpp in saftlib.h and saftbus.h

--- src/interfaces/CommonFunctions.h
+++ src/interfaces/CommonFunctions.h
@@ -1,6 +1,7 @@
 #ifndef COMMON_FUNCTIONS_COMPATIBILITY_H_
 #define COMMON_FUNCTIONS_COMPATIBILITY_H_
 
+#include <saftbus/error.hpp>
 #include <CommonFunctions.hpp>
 
 #endif
--- src/interfaces/saftbus.h
+++ src/interfaces/saftbus.h
@@ -1,5 +1,5 @@
-#ifndef SAFTLIB_COMPATIBILITY_H_
-#define SAFTLIB_COMPATIBILITY_H_
+#ifndef SAFTBUS_COMPATIBILITY_H_
+#define SAFTBUS_COMPATIBILITY_H_
 
 #include <saftbus/error.hpp>
 #include <saftbus/saftbus.hpp>
