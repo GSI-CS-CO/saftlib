GSI-CS-CO/saftlib

commit b7c3b7e1e279f07cb7042ee07d3231be6798486e

saft-io-clt and saft-pps-gen performance tweaks

src/saft-io-ctl.cpp
@@ -55,6 +55,27 @@
 #include "eca_flags.h"
 #include "io_control_regs.h"
 
+/* Hanle SIGINT */
+/* ==================================================================================================== */
+/* This program creates a lot of conditions an if it suddenly quits                                     */
+/* All conditions will be cleaned-up by the saftd at once. This can take a while                        */
+/* and causes the saftd to be unresponsive until all conditions are removed                             */
+/* this can be avoided by removing them one by one in the handler.                                      */
+#include <csignal>
+#include <cstdlib>
+std::vector<std::shared_ptr<saftlib::OutputCondition_Proxy> > all_output_conditions;
+std::vector<std::shared_ptr<saftlib::SoftwareCondition_Proxy> > all_software_conditions;
+void INThandler(int s) {
+  std::cerr << "removing all conditions" << std::endl;
+  for (auto& condition: all_output_conditions) {
+    condition->Destroy();
+  }
+  for (auto& condition: all_software_conditions) {
+    condition->Destroy();
+  }
+  exit(1); 
+}
+
 /* Namespace */
 /* ==================================================================================================== */
 using namespace saftlib;
@@ -151,12 +172,15 @@ static int io_create (bool disown, uint64_t eventID, uint64_t eventMask, int64_t
 
     /* Setup condition */
     std::shared_ptr<OutputCondition_Proxy> condition;
-    if (translate_mask) { condition = OutputCondition_Proxy::create(output_proxy->NewCondition(true, eventID, tr_mask(eventMask), io_offset, io_edge)); }
-    else                { condition = OutputCondition_Proxy::create(output_proxy->NewCondition(true, eventID, eventMask, io_offset, io_edge)); }
+    if (translate_mask) { condition = OutputCondition_Proxy::create(output_proxy->NewCondition(false, eventID, tr_mask(eventMask), io_offset, io_edge)); }
+    else                { condition = OutputCondition_Proxy::create(output_proxy->NewCondition(false, eventID, eventMask, io_offset, io_edge)); }
     condition->setAcceptConflict(io_AcceptConflict);
     condition->setAcceptDelayed(io_AcceptDelayed);
     condition->setAcceptEarly(io_AcceptEarly);
     condition->setAcceptLate(io_AcceptLate);
+    all_output_conditions.push_back(condition);
+
+    output_proxy->ToggleActive();
 
     /* Disown and quit or keep waiting */
     if (disown) { condition->Disown(); }
@@ -559,7 +583,7 @@ static int io_snoop(bool mode, bool setup_only, bool disable_source, uint64_t pr
 {
   /* Helpers (connect proxies in a vector) */
   std::vector <std::shared_ptr<SoftwareCondition_Proxy> > proxies;
-  std::vector <std::shared_ptr<SoftwareActionSink_Proxy> > sinks;
+  std::shared_ptr<SoftwareActionSink_Proxy>  sink;
 
   /* Get inputs and snoop */
   try
@@ -583,13 +607,14 @@ static int io_snoop(bool mode, bool setup_only, bool disable_source, uint64_t pr
         {
           if (!setup_only)
           {
-            sinks.push_back( SoftwareActionSink_Proxy::create(receiver->NewSoftwareActionSink("")));
-            proxies.push_back( SoftwareCondition_Proxy::create(sinks.back()->NewCondition(true, prefix, -2, IO_CONDITION_OFFSET)));
+            if (!sink) sink = SoftwareActionSink_Proxy::create(receiver->NewSoftwareActionSink(""));
+            proxies.push_back( SoftwareCondition_Proxy::create(sink->NewCondition(false, prefix, -2, IO_CONDITION_OFFSET)));
             proxies.back()->SigAction.connect(sigc::ptr_fun(&io_catch_input));
             proxies.back()->setAcceptConflict(true);
             proxies.back()->setAcceptDelayed(true);
             proxies.back()->setAcceptEarly(true);
             proxies.back()->setAcceptLate(true);
+            all_software_conditions.push_back(proxies.back());
           }
 
           /* Setup the event */
@@ -614,6 +639,7 @@ static int io_snoop(bool mode, bool setup_only, bool disable_source, uint64_t pr
         }
       }
     }
+    sink->ToggleActive();
 
     /* Disabled? */
     if (mode) { return (__IO_RETURN_SUCCESS); }
@@ -1088,6 +1114,12 @@ static int io_print_table(bool verbose_mode)
 /* ==================================================================================================== */
 int main (int argc, char** argv)
 {
+  struct sigaction sigIntHandler;
+  sigIntHandler.sa_handler = INThandler;
+  sigemptyset(&sigIntHandler.sa_mask);
+  sigIntHandler.sa_flags = 0;
+  sigaction(SIGINT, &sigIntHandler, NULL);
+
   /* Helpers to deal with endless arguments */
   char * pEnd         = NULL;  /* Arguments parsing */
   int  opt            = 0;     /* Number of given options */

src/saft-pps-gen.cpp
@@ -38,6 +38,31 @@
 using namespace saftlib;
 using namespace std;
 
+/* Hanle SIGINT */
+/* ==================================================================================================== */
+/* This program creates a lot of conditions an if it suddenly quits                                     */
+/* All conditions will be cleaned-up by the saftd at once. This can take a while                        */
+/* and causes the saftd to be unresponsive until all conditions are removed                             */
+/* this can be avoided by removing them one by one in the handler.                                      */
+#include <csignal>
+#include <cstdlib>
+std::vector<std::shared_ptr<saftlib::OutputCondition_Proxy> > all_output_conditions;
+std::vector<std::shared_ptr<saftlib::SoftwareCondition_Proxy> > all_software_conditions;
+std::vector<std::shared_ptr<saftlib::SCUbusCondition_Proxy> > all_scubus_conditions;
+void INThandler(int s) {
+  std::cerr << "removing all  conditions" << std::endl;
+  for (auto& condition: all_output_conditions) {
+    condition->Destroy();
+  }
+  for (auto& condition: all_software_conditions) {
+    condition->Destroy();
+  }
+  for (auto& condition: all_scubus_conditions) {
+    condition->Destroy();
+  }
+  exit(1); 
+}
+
 /* Global */
 /* ==================================================================================================== */
 static const char *program    = NULL;  /* Name of the application */
@@ -124,6 +149,12 @@ static void pps_help (void)
 /* ==================================================================================================== */
 int main (int argc, char** argv)
 {
+  struct sigaction sigIntHandler;
+  sigIntHandler.sa_handler = INThandler;
+  sigemptyset(&sigIntHandler.sa_mask);
+  sigIntHandler.sa_flags = 0;
+  sigaction(SIGINT, &sigIntHandler, NULL);
+
   /* Helpers */
   int  opt              = 0;     /* Number of given options */
   int  total_ios        = 0;     /* Number of configured IOs */
@@ -250,8 +281,8 @@ int main (int argc, char** argv)
           }
           
           /* Setup conditions */
-          std::shared_ptr<OutputCondition_Proxy> condition_high = OutputCondition_Proxy::create(output_proxy->NewCondition(true, ECA_EVENT_ID, ECA_EVENT_MASK, 0,         true));
-          std::shared_ptr<OutputCondition_Proxy> condition_low  = OutputCondition_Proxy::create(output_proxy->NewCondition(true, ECA_EVENT_ID, ECA_EVENT_MASK, 100000000, false));
+          std::shared_ptr<OutputCondition_Proxy> condition_high = OutputCondition_Proxy::create(output_proxy->NewCondition(false, ECA_EVENT_ID, ECA_EVENT_MASK, 0,         true));
+          std::shared_ptr<OutputCondition_Proxy> condition_low  = OutputCondition_Proxy::create(output_proxy->NewCondition(false, ECA_EVENT_ID, ECA_EVENT_MASK, 100000000, false));
           
           /* Accept all kinds of events */
           condition_high->setAcceptConflict(true);
@@ -262,6 +293,11 @@ int main (int argc, char** argv)
           condition_low->setAcceptDelayed(true);
           condition_low->setAcceptEarly(true);
           condition_low->setAcceptLate(true);
+
+          all_output_conditions.push_back(condition_low);
+          all_output_conditions.push_back(condition_high);
+
+          output_proxy->ToggleActive();
         }
       }
       
@@ -282,13 +318,17 @@ int main (int argc, char** argv)
        /* Get connection */
        std::shared_ptr<SCUbusActionSink_Proxy> e_scubus = SCUbusActionSink_Proxy::create(e_scubusses.begin()->second);
        std::shared_ptr<SCUbusCondition_Proxy> scubus_condition;
-       scubus_condition = SCUbusCondition_Proxy::create(e_scubus->NewCondition(true, ECA_EVENT_ID, ECA_EVENT_MASK, 0, scu_bus_tag));
+       scubus_condition = SCUbusCondition_Proxy::create(e_scubus->NewCondition(false, ECA_EVENT_ID, ECA_EVENT_MASK, 0, scu_bus_tag));
         
        /* Accept every kind of event */
        scubus_condition->setAcceptConflict(true);
        scubus_condition->setAcceptDelayed(true);
        scubus_condition->setAcceptEarly(true);
        scubus_condition->setAcceptLate(true);
+
+       all_scubus_conditions.push_back(scubus_condition);
+
+       e_scubus->ToggleActive();
        std::cout << "ECA configuration done for SCU bus!" << std::endl;
       }
       
@@ -331,7 +371,7 @@ int main (int argc, char** argv)
         /* Setup SoftwareActionSink */
         std::cout << "Waiting for timing events..." << std::endl;
         std::shared_ptr<SoftwareActionSink_Proxy> sink = SoftwareActionSink_Proxy::create(receiver->NewSoftwareActionSink(""));
-        std::shared_ptr<SoftwareCondition_Proxy> condition = SoftwareCondition_Proxy::create(sink->NewCondition(true, ECA_EVENT_ID, ECA_EVENT_MASK, 0));
+        std::shared_ptr<SoftwareCondition_Proxy> condition = SoftwareCondition_Proxy::create(sink->NewCondition(false, ECA_EVENT_ID, ECA_EVENT_MASK, 0));
         condition->SigAction.connect(sigc::bind(sigc::ptr_fun(&onAction), 0));
         
         /* Accept all kinds of events */
@@ -339,6 +379,10 @@ int main (int argc, char** argv)
         condition->setAcceptDelayed(true);
         condition->setAcceptEarly(true);
         condition->setAcceptLate(true);
+
+        all_software_conditions.push_back(condition);
+
+        sink->ToggleActive();
         
         /* Attach to counter signals */
         sink->OverflowCount.connect(sigc::ptr_fun(&onOverflowCount));

commit b92bb542db6095426b64ccc877595acb71a81719

saft-io-ctl: fix creation of conditions
There was a problem introduced in commit b7c3b7e1e when I tried to optimize the performance of the tool.
The optimization had the side effect that after creating N conditions with saft-io-ctl every second condition
was not active.

src/saft-io-ctl.cpp
@@ -153,14 +153,12 @@ static int io_create (bool disown, uint64_t eventID, uint64_t eventMask, int64_t
 
     /* Setup condition */
     std::shared_ptr<OutputCondition_Proxy> condition;
-    if (translate_mask) { condition = OutputCondition_Proxy::create(output_proxy->NewCondition(false, eventID, tr_mask(eventMask), io_offset, io_edge)); }
-    else                { condition = OutputCondition_Proxy::create(output_proxy->NewCondition(false, eventID, eventMask, io_offset, io_edge)); }
+    if (translate_mask) { condition = OutputCondition_Proxy::create(output_proxy->NewCondition(true, eventID, tr_mask(eventMask), io_offset, io_edge)); }
+    else                { condition = OutputCondition_Proxy::create(output_proxy->NewCondition(true, eventID, eventMask, io_offset, io_edge)); }
     condition->setAcceptConflict(io_AcceptConflict);
     condition->setAcceptDelayed(io_AcceptDelayed);
     condition->setAcceptEarly(io_AcceptEarly);
     condition->setAcceptLate(io_AcceptLate);
-
-    output_proxy->ToggleActive();
 
     /* Disown and quit or keep waiting */
     if (disown) { condition->Disown(); }