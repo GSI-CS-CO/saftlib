a901702f84ec0b4e194a77c0db8a1da835b96b7d
Michael Reese <m.reese@gsi.de>
2023-11-24 10:22:30 +0100

    add a bunch of missing compatibility header files

---
+++ src/SignalGroup.h
@@ -0,0 +1,7 @@
+#ifndef SIGNALGROUP_COMPATIBILITY_H_
+#define SIGNALGROUP_COMPATIBILITY_H_
+
+#include <saftbus/error.hpp>
+#include <CommonFunctions.hpp>
+
+#endif
---
+++ src/Time.h
@@ -0,0 +1,8 @@
+#ifndef TIME_COMPATIBILITY_H_
+#define TIME_COMPATIBILITY_H_
+
+#include <saftbus/error.hpp>
+#include <Time.hpp>
+#include <CommonFunctions.hpp>
+
+#endif
