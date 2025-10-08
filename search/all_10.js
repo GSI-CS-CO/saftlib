0468375392782b69c407551e2e7d88463b60dcd5
anragsi <anragsi@users.noreply.github.com>
2025-10-08 12:33:51 +0000

    deploy: 8dae2cafad0d1e7b52997a5a845d9b3debd5141c

--- search/all_10.js
+++ search/all_10.js
@@ -1,9 +1,11 @@
 var searchData=
 [
-  ['tempsensor_0',['TempSensor',['../classsaftlib_1_1TempSensor.html',1,'saftlib']]],
-  ['time_1',['Time',['../classsaftlib_1_1Time.html',1,'saftlib']]],
-  ['timeoutsource_2',['TimeoutSource',['../classsaftbus_1_1TimeoutSource.html',1,'saftbus::TimeoutSource'],['../classsaftbus_1_1TimeoutSource.html#aa5fd3a7dfd744e6792852d095229a6e1',1,'saftbus::TimeoutSource::TimeoutSource(std::function&lt; bool(void)&gt; slot, std::chrono::milliseconds interval, std::chrono::milliseconds offset)'],['../classsaftbus_1_1TimeoutSource.html#ad65d0531b3db2817004393a14ff14d43',1,'saftbus::TimeoutSource::TimeoutSource(std::function&lt; bool(void)&gt; slot, std::chrono::milliseconds interval)']]],
-  ['timingreceiver_3',['TimingReceiver',['../classsaftlib_1_1TimingReceiver.html',1,'saftlib']]],
-  ['timingreceiveraddon_4',['TimingReceiverAddon',['../classsaftlib_1_1TimingReceiverAddon.html',1,'saftlib']]],
-  ['toggleactive_5',['ToggleActive',['../classsaftlib_1_1ActionSink.html#ac3596e1d3db031efb531a49648fe524f',1,'saftlib::ActionSink::ToggleActive()'],['../classsaftlib_1_1ECA.html#a2a9679fc47934de5cb9a0b3aa4171c4d',1,'saftlib::ECA::ToggleActive()']]]
+  ['tai_5fis_5futcleap_0',['TAI_is_UTCleap',['../namespacesaftlib.html#a36fc12cd3fb1a6771450ed5b889f5f45',1,'saftlib']]],
+  ['tai_5fto_5futc_1',['TAI_to_UTC',['../namespacesaftlib.html#a021674a69fbeb19ba314ddaf7612365e',1,'saftlib']]],
+  ['tempsensor_2',['TempSensor',['../classsaftlib_1_1TempSensor.html',1,'saftlib']]],
+  ['time_3',['Time',['../classsaftlib_1_1Time.html',1,'saftlib']]],
+  ['timeoutsource_4',['TimeoutSource',['../classsaftbus_1_1TimeoutSource.html',1,'saftbus::TimeoutSource'],['../classsaftbus_1_1TimeoutSource.html#aa5fd3a7dfd744e6792852d095229a6e1',1,'saftbus::TimeoutSource::TimeoutSource(std::function&lt; bool(void)&gt; slot, std::chrono::milliseconds interval, std::chrono::milliseconds offset)'],['../classsaftbus_1_1TimeoutSource.html#ad65d0531b3db2817004393a14ff14d43',1,'saftbus::TimeoutSource::TimeoutSource(std::function&lt; bool(void)&gt; slot, std::chrono::milliseconds interval)']]],
+  ['timingreceiver_5',['TimingReceiver',['../classsaftlib_1_1TimingReceiver.html',1,'saftlib']]],
+  ['timingreceiveraddon_6',['TimingReceiverAddon',['../classsaftlib_1_1TimingReceiverAddon.html',1,'saftlib']]],
+  ['toggleactive_7',['ToggleActive',['../classsaftlib_1_1ActionSink.html#ac3596e1d3db031efb531a49648fe524f',1,'saftlib::ActionSink::ToggleActive()'],['../classsaftlib_1_1ECA.html#a2a9679fc47934de5cb9a0b3aa4171c4d',1,'saftlib::ECA::ToggleActive()']]]
 ];
