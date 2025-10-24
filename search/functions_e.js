8f0be02f688117c9cedb47edbeaafeb265c887ff
alyxazon <alyxazon@users.noreply.github.com>
2025-10-24 09:52:38 +0000

    deploy: 54abf95d731b811c0d50bfc50d7a707566b3d1c1

--- search/functions_e.js
+++ search/functions_e.js
@@ -3,9 +3,5 @@ var searchData=
   ['unregister_5fproxy_0',['unregister_proxy',['../classsaftbus_1_1SignalGroup.html#a21fa58f05573625e3515d17ae56bfe9a',1,'saftbus::SignalGroup']]],
   ['unregister_5fsignal_5fid_5ffor_5fclient_1',['unregister_signal_id_for_client',['../classsaftbus_1_1ServerConnection.html#a3855dc28070d74acddf573594300dac6',1,'saftbus::ServerConnection']]],
   ['use_2',['Use',['../classsaftlib_1_1Mailbox_1_1Slot.html#a94efd17810a7b01480259de4b0060767',1,'saftlib::Mailbox::Slot']]],
-  ['useslot_3',['UseSlot',['../classsaftlib_1_1Mailbox.html#a5dc230a985a8d63255d7aee7f207641f',1,'saftlib::Mailbox']]],
-  ['utc_5fdifference_4',['UTC_difference',['../namespacesaftlib.html#a60acd4708b95d5b38afdd8dee8da2f85',1,'saftlib']]],
-  ['utc_5foffset_5ftai_5',['UTC_offset_TAI',['../namespacesaftlib.html#a4f63f297b1cbd14e930a57219b73e138',1,'saftlib']]],
-  ['utc_5foffset_5futc_6',['UTC_offset_UTC',['../namespacesaftlib.html#acf7a4afb73a0f146ef3045d67e7e8332',1,'saftlib']]],
-  ['utc_5fto_5ftai_7',['UTC_to_TAI',['../namespacesaftlib.html#a4fdba7c54aff36459ce2cfca4004bfe2',1,'saftlib']]]
+  ['useslot_3',['UseSlot',['../classsaftlib_1_1Mailbox.html#a5dc230a985a8d63255d7aee7f207641f',1,'saftlib::Mailbox']]]
 ];
