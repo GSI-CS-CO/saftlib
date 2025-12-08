d9a5357293c80ba2a965cd55ffc4bfb38cc34da0
Sharian <Sharian@users.noreply.github.com>
2025-12-08 12:53:31 +0000

    deploy: a6aef33a2b1ba55d5bf3411fa6e067dfac619f0b

--- search/functions_d.js
+++ search/functions_d.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['tai_5fis_5futcleap_0',['TAI_is_UTCleap',['../namespacesaftlib.html#a36fc12cd3fb1a6771450ed5b889f5f45',1,'saftlib']]],
-  ['tai_5fto_5futc_1',['TAI_to_UTC',['../namespacesaftlib.html#a021674a69fbeb19ba314ddaf7612365e',1,'saftlib']]],
-  ['timeoutsource_2',['TimeoutSource',['../classsaftbus_1_1TimeoutSource.html#aa5fd3a7dfd744e6792852d095229a6e1',1,'saftbus::TimeoutSource::TimeoutSource(std::function&lt; bool(void)&gt; slot, std::chrono::milliseconds interval, std::chrono::milliseconds offset)'],['../classsaftbus_1_1TimeoutSource.html#ad65d0531b3db2817004393a14ff14d43',1,'saftbus::TimeoutSource::TimeoutSource(std::function&lt; bool(void)&gt; slot, std::chrono::milliseconds interval)']]],
-  ['toggleactive_3',['ToggleActive',['../classsaftlib_1_1ActionSink.html#ac3596e1d3db031efb531a49648fe524f',1,'saftlib::ActionSink::ToggleActive()'],['../classsaftlib_1_1ECA.html#a2a9679fc47934de5cb9a0b3aa4171c4d',1,'saftlib::ECA::ToggleActive()']]]
+  ['timeoutsource_0',['TimeoutSource',['../classsaftbus_1_1TimeoutSource.html#aa5fd3a7dfd744e6792852d095229a6e1',1,'saftbus::TimeoutSource::TimeoutSource(std::function&lt; bool(void)&gt; slot, std::chrono::milliseconds interval, std::chrono::milliseconds offset)'],['../classsaftbus_1_1TimeoutSource.html#ad65d0531b3db2817004393a14ff14d43',1,'saftbus::TimeoutSource::TimeoutSource(std::function&lt; bool(void)&gt; slot, std::chrono::milliseconds interval)']]],
+  ['toggleactive_1',['ToggleActive',['../classsaftlib_1_1ActionSink.html#ac3596e1d3db031efb531a49648fe524f',1,'saftlib::ActionSink::ToggleActive()'],['../classsaftlib_1_1ECA.html#a2a9679fc47934de5cb9a0b3aa4171c4d',1,'saftlib::ECA::ToggleActive()']]]
 ];
